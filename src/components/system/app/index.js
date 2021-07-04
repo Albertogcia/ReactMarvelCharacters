42edefbf083e09afaa224e71095b3c7911ad419b
Alberto García Antuña <garantalberto@gmail.com>
2021-07-04 12:29:53 +0200

    Initial commit

---
+++ src/components/system/app/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Router, Scene, Stack} from 'react-native-router-flux';
+
+class App extends Component {
+  render() {
+    return (
+        
+    );
+  }
+}
+
+export default App;
