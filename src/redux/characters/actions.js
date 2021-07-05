af8731072fc31eb3cb57d39e47941f81b76d09a7
Alberto García Antuña <garantalberto@gmail.com>
2021-07-05 20:37:26 +0200

    Redux characters added

---
+++ src/redux/characters/actions.js
@@ -0,0 +1,45 @@
+import {Alert} from 'react-native';
+import * as types from './types';
+import * as api from '../../api';
+
+export const setLoading = (loading = false) => {
+  const action = {
+    type: types.UPDATE_CHARACTERS_LOADING,
+    payload: {loading},
+  };
+  return action;
+};
+
+export const setList = (list = []) => {
+  const action = {
+    type: types.UPDATE_CHARACTERS_LIST,
+    payload: {list},
+  };
+  return action;
+};
+
+export const setItem = item => {
+  const action = {
+    type: types.UPDATE_CHARACTERS_ITEM,
+    payload: {item},
+  };
+  return action;
+};
+
+export const getList = () => {
+    return async (dispatch, getState) => {
+      try {
+        dispatch(setLoading(true));
+
+        const charactersResponse = await api.getCharacters(0,20);
+        
+        const list = charactersResponse.data?.data?.results || [];
+  
+        dispatch(setList(list))
+      } catch (e) {
+        Alert.alert('Error', e.message || 'Ha ocurrido un error obteniendo los datos');
+      } finally {
+        dispatch(setLoading(false));
+      }
+    };
+  };
---
+++ src/redux/characters/reducer.js
@@ -0,0 +1,34 @@
+import * as types from './types';
+
+const initialState = {
+  loading: false,
+  list: [],
+  item: null,
+};
+
+const reducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case types.UPDATE_CHARACTERS_LIST:
+      return {
+        ...state,
+        list: action.payload?.list,
+      };
+
+    case types.UPDATE_CHARACTERS_LOADING:
+      return {
+        ...state,
+        loading: action.payload?.loading,
+      };
+
+    case types.UPDATE_CHARACTERS_ITEM:
+      return {
+        ...state,
+        item: action.payload?.item,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
