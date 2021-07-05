19ac5705a023102efce11747dcd00ff578149722
Alberto García Antuña <garantalberto@gmail.com>
2021-07-05 20:37:42 +0200

    Added api constants

---
+++ src/config/api.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://gateway.marvel.com';
+export const API_KEY = 'b737535827f8f99c27862e69d24395bc';
+export const API_HASH = 'f234efb247341f3b02a0552df7c7983b';
+export const API_TS = '1';
